f705043ef25c8513ed159b58a6c4619637c33b96
Deepak675-max <deepakkamboj.kamboj.675@gmail.com>
2024-04-08 11:11:12 +0530

    updated api gateway

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const proxy = require("express-http-proxy");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", proxy("http://localhost:8003"));
 app.use("/order", proxy("http://localhost:8006"));
 app.use("/product", proxy("http://localhost:8004"));
 app.use("/cart", proxy("http://localhost:8005"));
-
+app.use("/notification", proxy("http://localhost:8007"));
 
 const port = 8000;
 
